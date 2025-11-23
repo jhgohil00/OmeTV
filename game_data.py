af0adbb0ddd76866edbbcb5cf2a4ae2e2521cdac
jhgohil00 <jhgohil0045@gmail.com>
2025-11-23 16:59:16 +0530

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,32 @@
+# 📚 GAME CONTENT LIBRARY (PHASE 18)
+
+GAME_DATA = {
+    "tod_truth": [
+        "What is your biggest fear?", "What is the last lie you told?", "Who is your secret crush?",
+        "What is your most embarrassing moment?", "Have you ever cheated on a test?",
+        "What is the worst gift you ever received?", "What is your biggest regret?",
+        "When was the last time you cried?", "What is a secret you've never told anyone?",
+        "If you could switch lives with one person, who would it be?",
+        "What is a habit you want to break?", "Who is the most annoying person you know?",
+        "What is your biggest insecurity?"
+    ],
+    "tod_dare": [
+        "Send a voice note singing 'Happy Birthday'.", "Send the 3rd photo in your gallery.",
+        "Type a message with your nose.", "Send a sticker that describes you.",
+        "Do 10 pushups and send a video note.",
+        "Talk in emojis for the next 3 turns.", "Describe your crush without naming them.",
+        "Send a screenshot of your home screen.",
+        "Send a voice note whispering a secret."
+    ],
+    "wyr": [
+        ("Have infinite money", "Have infinite love"),
+        ("Be invisible", "Be able to fly"), 
+        ("Always be cold", "Always be hot"),
+        ("Have unlimited money", "Have unlimited time"), 
+        ("Know how you die", "Know when you die"),
+        ("Explore Space", "Explore the Ocean"), 
+        ("Talk to animals", "Speak all languages"),
+        ("Be a famous movie star", "Be a brilliant scientist"),
+        ("Never use social media again", "Never watch movies again")
+    ]
+}
